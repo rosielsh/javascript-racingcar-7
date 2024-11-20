ff7fde312a2c9f7c8fa2e0150001e6586bb2ddf9
suhwa <tnghk9611@naver.com>
2024-11-21 00:30:06 +0900

    test(CarsTest): 전진하지 않는 테스트 구현

--- __tests__/models/Cars.test.js
+++ __tests__/models/Cars.test.js
@@ -25,4 +25,20 @@ describe("Cars 클래스 테스트", () => {
       expect(p).toEqual(expectedPos[idx]);
     });
   });
+
+  test("자동차들이 모두 4 미만의 수를 가지면 모두 전진하지 않는다.", () => {
+    // given
+    const names = ["자동차1", "자동차2", "자동차3"];
+    const cars = new Cars(names);
+    const expectedPos = [0, 0, 0];
+
+    // when
+    mockRandoms([2, 1, 3]);
+    const pos = cars.race();
+
+    // then
+    pos.forEach((p, idx) => {
+      expect(p).toEqual(expectedPos[idx]);
+    });
+  });
 });
