4c3923ae2b8acc7c402aef6b7edf28437eb38745
suhwa <tnghk9611@naver.com>
2024-11-21 00:28:45 +0900

    test(CarsTest): 생성자 테스트 삭제
    - 2번째 테스트가 1번째 테스트에서 Car를 mocking한 문제로 정상적인 테스트 동작x

--- __tests__/models/Cars.test.js
+++ __tests__/models/Cars.test.js
@@ -1,8 +1,5 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import Car from "../../src/models/Car";
 import Cars from "../../src/models/Cars";
-
-jest.mock("../../src/models/Car.js", () => jest.fn());
 
 const mockRandoms = (numbers) => {
   MissionUtils.Random.pickNumberInRange = jest.fn();
@@ -13,17 +10,6 @@ const mockRandoms = (numbers) => {
 };
 
 describe("Cars 클래스 테스트", () => {
-  test("자동차는 입력받은 이름 갯수만큼 생성된다.", () => {
-    // given
-    const names = ["자동차1", "자동차2", "자동차3"];
-
-    // when
-    new Cars(names);
-
-    // then
-    expect(Car).toHaveBeenCalledTimes(3);
-  });
-
   test("자동차들이 모두 4의 랜덤수를 가지면 모두 전진한다.", () => {
     // given
     const names = ["자동차1", "자동차2", "자동차3"];
@@ -32,7 +18,6 @@ describe("Cars 클래스 테스트", () => {
 
     // when
     mockRandoms([4, 4, 4]);
-
     const pos = cars.race();
 
     // then
