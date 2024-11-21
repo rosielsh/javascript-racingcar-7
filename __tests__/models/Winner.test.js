b4c80312a61378c6fa3e8dcefb094bc076724464
suhwa <tnghk9611@naver.com>
2024-11-21 22:15:31 +0900

    test(WinnerTest): 공동으로 우승하는 테스트 구현

--- __tests__/models/Winner.test.js
+++ __tests__/models/Winner.test.js
@@ -32,4 +32,27 @@ describe("Winner 클래스 테스트", () => {
     // then
     expect(winner.getWinners()[0]).toEqual(winnerName);
   });
+
+  test("우승자는 한 명 이상일 수 있다.", () => {
+    // given
+    const carNames = ["자동차1", "자동차2", "자동차3"];
+    const cars = new Cars(carNames, RaceStrategy);
+    const tryCount = 2;
+    const randomNum = [6, 6, 0, 6, 6, 0];
+    const expectedWinners = [carNames[0], carNames[1]];
+
+    // when
+    mockRandoms(randomNum);
+
+    Array.from({ length: tryCount }).forEach(() => {
+      cars.race();
+    });
+
+    const winner = Winner.from(cars.getCars());
+    const winners = winner.getWinners();
+
+    // then
+    expect(winners.length).toEqual(2);
+    expect(winners).toEqual(expectedWinners);
+  });
 });
