6bb18a48e9e4cec9ce1fe6d9e4cfeef130ae7245
suhwa <tnghk9611@naver.com>
2024-11-22 16:46:45 +0900

    fix(InputView): 입력 과정에서 비동기 처리

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,16 +1,16 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class InputView {
-  static readCarNames() {
-    return this.read("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+  static async readCarNames() {
+    return await this.#read("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
   }
 
-  static readTryCount() {
-    return this.read("시도할 횟수는 몇 회인가요?\n");
+  static async readTryCount() {
+    return await this.#read("시도할 횟수는 몇 회인가요?\n");
   }
 
-  read(prompt) {
-    return Console.readLineAsync(prompt);
+  static async #read(prompt) {
+    return await Console.readLineAsync(prompt);
   }
 }
 
