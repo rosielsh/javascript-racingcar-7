35ed4267f89545c3ce4ad2a4f492850b017c0e62
suhwa <tnghk9611@naver.com>
2024-11-22 17:17:59 +0900

    refactor(RaceController): 함수 10줄 미만 요구사항 반영

--- src/controller/RaceController.js
+++ src/controller/RaceController.js
@@ -10,21 +10,40 @@ import OutputView from "../views/OutputView.js";
 
 class RaceController {
   async race() {
+    const carNames = await this.#getCarNames();
+    const tryCount = await this.#getTryCount();
+
+    const cars = this.#race(carNames, tryCount);
+    this.#calculateWinner(cars);
+  }
+
+  async #getCarNames() {
     const carNames = await InputView.readCarNames();
     const parsedCarNames = InputParser.parseCarNames(carNames);
     CarNameValidator.validate(parsedCarNames);
 
+    return parsedCarNames;
+  }
+
+  async #getTryCount() {
     const tryCount = await InputView.readTryCount();
     const convertedTryCount = Converter.convertTryCountStringToNumber(tryCount);
     TryCountValidator.validate(convertedTryCount);
+    return convertedTryCount;
+  }
 
-    const cars = new Cars(parsedCarNames, RaceStrategy);
+  #race(carNames, tryCount) {
+    const cars = new Cars(carNames, RaceStrategy);
     OutputView.printResultText();
-    Array.from({ length: convertedTryCount }).forEach(() => {
+    Array.from({ length: tryCount }).forEach(() => {
       const currentRaceInfo = cars.race();
       OutputView.printRaceResult(currentRaceInfo);
     });
 
+    return cars;
+  }
+
+  async #calculateWinner(cars) {
     const winner = Winner.from(cars.getCars());
     OutputView.printWinners(winner.getWinners());
   }
